93210739065056e78ece5182651576f59d14e4f9
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-14 14:54:38 -0500

    Updated from Brython Server: 12/14/2017 2:54:38 PM

--- finalProject.py
+++ finalProject.py
@@ -1,1 +1,51 @@
+#Pedro Gallino
+#12/17/17
+#finalProject.py - snake game
+
+from ggame import *
+
+COLUMNS = 8
+ROWS = 8
+
+def loadSnakeBoard():
+    board = []
+    for i in range(COLUMNS):
+        board.append([0]*ROWS)
+    board[0][0] = 1
+    board[1][1] = -1
+    data['board'] = board
+
+def redrawAll:
+    for item in App().spritelist[:]:
+        item.destroy()
+
+
+if __name__ == '__main__':
+    data = {}
+    loadSnakeBoard()
+    print(data['board'])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
