7df6945c4659ef48818d29344e1c213a7176f386
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-15 14:50:19 -0500

    Updated from Brython Server: 12/15/2017 2:50:18 PM

--- finalProject.py
+++ finalProject.py
@@ -7,8 +7,9 @@ from ggame import *
 COLUMNS = 8
 ROWS = 8
 GREEN = Color(0x00ff00,1)
-BLUE = Color(0x000ff,1)
-BLACK = Color(0x00000,1)
+BLUE = Color(0x0000ff,1)
+BLACK = Color(0x000000,1)
+WHITE = Color(0xFFFFFF,1)
 
 def loadSnakeBoard():
     board = []
@@ -18,14 +19,23 @@ def loadSnakeBoard():
     board[1][1] = -1
     data['board'] = board
 
-def redrawAll:
+def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
-
     drawSnakeBoard()
-    if data['gameOver'] = True
+    if data['gameOver'] == True:
         gameOverText = TextAsset('You LOSE', fill=BLACK, style='bold 40pt Times')
-        Sprite(gameOverText,(500,500))
+        Sprite(gameOverText,(500,300))
+
+def drawSnakeBoard():
+    for cell in data['board']:
+        drawSnakeCell(ROWS,COLUMNS)
+
+def drawSnakeCell(rows,columns):
+    square = RectangleAsset(100,100,LineStyle(10,BLACK),WHITE)
+    Sprite(square,(175,100))
+    
+
 
 if __name__ == '__main__':
     
@@ -33,7 +43,8 @@ if __name__ == '__main__':
     data['gameOver'] = False
     
     loadSnakeBoard()
-    
+    drawSnakeCell(ROWS,COLUMNS)
+
 
 
 
